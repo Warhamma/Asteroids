4dd4a9a2dd7bae84c4a4cf50c76872042e961d98
hpt.hamma <hpt.hamma@gmail.com>
2026-06-08 17:24:57 +1000

    D: Created player ship

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 
-from constants import SCREEN_WIDTH,  SCREEN_HEIGHT
+from constants import SCREEN_WIDTH,  SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -14,13 +16,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
+
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
